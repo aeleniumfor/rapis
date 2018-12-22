a76df61412f60bda8a3ee713a540fc0a89916e39
fukuda <fukudammk@gmail.com>
2018-12-22 15:41:02 +0900

    sqls/main.go

--- initsql/init.sql
+++ initsql/init.sql
@@ -5,7 +5,7 @@ CREATE DATABASE domain;
 CREATE TABLE hosts (
   id                 SERIAL PRIMARY KEY NOT NULL,
   project_name       VARCHAR(256)       NOT NULL UNIQUE,
-  r_urls             VARCHAR(256)       NOT NULL UNIQUE,
+  v_urls             VARCHAR(256)       NOT NULL UNIQUE,
   t_urls             VARCHAR(256)       NOT NULL UNIQUE,
   created_at         TIMESTAMP          NOT NULL DEFAULT current_timestamp,
   updated_at         TIMESTAMP          NOT NULL DEFAULT current_timestamp
--- sqls/main.go
+++ sqls/main.go
@@ -7,13 +7,13 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type Routing struct {
+type DB_Routing struct {
 	v_urls string
 	t_urls string
 	urls   string
 }
 
-func (r *Routing) Get_Routing() {
+func (r *DB_Routing) Get_Routing() {
 	db, err := sql.Open("postgres", "host=localhost port=5431 user=root password=root dbname=domain sslmode=disable")
 	defer db.Close()
 
@@ -21,23 +21,23 @@ func (r *Routing) Get_Routing() {
 		fmt.Println(err)
 	}
 
-	r.v_urls = "web.user.localhost"
-	query := fmt.Sprintf("SELECT r_urls FROM hosts WHERE p_urls = '%s'", r.v_urls)
+	r.v_urls = "testproject.user.localhost"
+	query := fmt.Sprintf("SELECT t_urls FROM hosts WHERE v_urls = '%s'", r.v_urls)
 	rows, err := db.Query(query)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	for rows.Next() {
-		rows.Scan(r.t_urls)
+		rows.Scan(&r.t_urls)
 	}
 
 	fmt.Println(r.v_urls + " -> " + r.t_urls)
 }
 
-func Init() *Routing {
+func Init() *DB_Routing {
 	fmt.Println("Init")
-	return &Routing{}
+	return &DB_Routing{}
 }
 
 func main() {
