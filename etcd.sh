6bb9726a21ba691d14ed917bf775c42bad96e2de
fukuda <fukudammk@gmail.com>
2018-12-26 23:29:10 +0900

    traefik

---
+++ etcd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+tip=http://$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rapis_etcd_1):2379
+echo $tip
+entry_1=http://$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rapis_test1_1):80
+entry_2=http://$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rapis_test2_1):80
+
+echo $entry_1
+echo $entry_2
+
+curl -XPUT $tip/v2/keys/traefik/backends/backend1/circuitbreaker/expression -d value='NetworkErrorRatio() > 0.5'
+
+curl -XPUT $tip/v2/keys/traefik/backends/backend1/servers/server1/url -d value=$entry_1
+curl -XPUT $tip/v2/keys/traefik/backends/backend1/servers/server2/url -d value=$entry_2
+
+curl -XPUT $tip/v2/keys/traefik/backends/backend1/servers/server1/weight -d value='5'
+curl -XPUT $tip/v2/keys/traefik/backends/backend1/servers/server2/weight -d value='5'
